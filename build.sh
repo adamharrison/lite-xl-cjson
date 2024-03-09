9d0a4f4df2df3abbe18149da71e9b71c41aac376
Adam Harrison <adamdharrison@gmail.com>
2024-03-08 23:19:34 -0500

    Simplfied paths.

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 : ${CC=gcc}
 : ${BIN=cjson.so}
 
-CFLAGS="$CFLAGS -Ilib/lite-xl/resources/include -Ilib/cJSON -fPIC -shared"
+CFLAGS="$CFLAGS -Ilib/lite-xl/resources/include -fPIC -shared"
 
 [[ "$@" == "clean" ]] && rm -rf $BIN && exit 0
 $CC $CFLAGS *.c -o $BIN $@
